0a1c9bf77802618c8cb9ea064ec814e1c7d5f14e
Colby <me@cobeh.com>
2022-02-20 14:46:30 -0800

    fixed bug

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo "Copying service file to /etc/systemd/system/"
 cp /root/website/website.service /etc/systemd/system/website.service
 echo "Reloading service files"
-sudo systemctl start your-service.service
+sudo systemctl start website.service
 echo "Enabling on reboot"
 sudo systemctl enable website.service
