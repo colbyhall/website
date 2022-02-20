487a9c2e136b55b8ce103a9900da20050eae8fe1
Colby <me@cobeh.com>
2022-02-20 14:51:17 -0800

    please

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 echo "Copying service file to /etc/systemd/system/"
 cp /root/website/website.service /etc/systemd/system/website.service
 echo "Reloading service files"
+sudo systemctl daemon-reload
+echo "Starting service"
 sudo systemctl start website.service
 echo "Enabling on reboot"
 sudo systemctl enable website.service
